8eca6b43a35d67af1201aaf638b62ec6f1aaa27d
Sylviaani <sylviaani26@gmail.com>
2024-05-02 12:14:49 +0100

    Unittests

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,12 @@
+function getPaymentTokenFromAPI(success) {
+  if (success === true) {
+    return new Promise((resolve, reject) => {
+      obj = { data: "Successful response from the API" };
+      resolve(obj);
+    });
+  } else {
+    // Do nothing
+  }
+}
+
+module.exports = getPaymentTokenFromAPI;
---
+++ 0x06-unittests_in_js/annotations.py
@@ -0,0 +1,7 @@
+def add(a: int, b: int):
+    """Adds two numbers"""
+    return a + b
+
+
+result = add("y", 2)
+print(result)
